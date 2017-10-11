9ca14c34a9d7a26481d9d13afd3573c0a07a45cf
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-10-11 18:45:11 +0200

    Lyrics to functional component

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Lyrics extends Component {
-
-  render() {
-    return (
-      <div className="jumbotron">
-        {
-          this.props.song &&
-          this.props.band &&
-          <h1>{ this.props.song } - { this.props.band }</h1>
-        }
-        {
-          this.props.lyrics &&
-          this.props.lyrics
-                .split('\n')
-                .filter( line => !line.includes('Paroles de la chanson'))
-                .map( line => <p>{ line }</p> )
-        }
-      </div>
-    )
-  }
-}
+const Lyrics = ({ lyrics, band, song }) => (
+  <div className="jumbotron">
+    {
+      song && band &&
+      <h1>{ song } - { band }</h1>
+    }
+    {
+      lyrics && lyrics.split('\n')
+                  .filter( line => !line.includes('Paroles de la chanson'))
+                  .map( line => <p>{ line }</p> )
+    }
+  </div>
+)
 
 export default Lyrics;
